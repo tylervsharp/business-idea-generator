4ae1ae0d32d0d480b4aba77bb9333401d4f94805
Tyler Sharp <tyler.v.sharp@gmail.com>
2016-10-20 17:23:58 -0400

    Add materialize to project. Restyle home, login, and registration pages accordingly.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,26 @@
 get '/' do
   erb :'index'
 end
+
+get '/login' do
+  erb :'sessions/new'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:user][:email])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    create_trends
+    redirect '/'
+  else
+    @errors = ["Invalid email or password"]
+    erb :'users/login'
+  end
+end
+
+get '/logout' do
+  if logged_in?
+    session.clear
+  end
+    redirect '/'
+end
